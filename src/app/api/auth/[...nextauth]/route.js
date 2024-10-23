418fbd81088fe6bd39d7c612fa4f4e26c16d8c10
Lewis-Allen-2001 <itslewbie01@gmail.com>
2024-10-23 14:28:38 +0100

    yes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
 import { handlers } from "@/auth";
 
-export const { GET, POST } = handlers;
+export const { auth, GET, POST } = NextAuth({ providers: [ GitHub ] })
